67e65215af6aacdf10c2ec72fbb6ec44f4832179
dac <dac@dypm.org>
2025-01-09 12:09:10 +0530

    multiplication added in this

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int Main() {
+
+    int a, b;
+    printf("Program for addtion ");
+    printf("Enter first number: ");
+    scanf("%d");
+    printf("Enter second number number: ");
+    scanf("%d");
+    
+    printf("Program for Multiplication ");
+    printf("Enter first number: ");
+    scanf("%d");
+    printf("Enter second number number: ");
+    scanf("%d");
+     printf("Addition is: ", (a+b));
+    printf("Multiplication is: ", (a*b));
+}
